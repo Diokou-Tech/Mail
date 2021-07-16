a75fc65867126a92f175b44cb43b3b93bb0d0e90
zola <diokoucheikh@gmail.com>
2021-07-16 17:41:06 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mail</title>
+</head>
+<body>
+    <h1>Envoyer un email</h1>
+    <h3><a href="mail.php">Envoyer</a> </h3>
+</body>
+<style>
+    body{
+        padding: 20px;
+        margin: 25px;
+        flex-direction: column;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+</html>
---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+// phpinfo();
+$Name = "Da Duder"; //senders name
+$email = "diokoucheikhou@gmail.com"; //senders e-mail adress
+$recipient = "diokoucheikh@gmail.com"; //recipient
+$mail_body = "The text for the mail..."; //mail body
+$subject = "Test email"; //subject
+$header = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields
+
+mail($recipient, $subject, $mail_body, $header); //mail command :)
+?>
